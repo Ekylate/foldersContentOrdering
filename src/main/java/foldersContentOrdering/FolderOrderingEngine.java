fee089b5bf58b5232c2b254c6fd0105a1b3aa410
Ekylate <jt.antoine@gmail.com>
2025-02-17 21:04:19 +0100

    [JTA/JAE] : Renommage de fichiers 2.0
    [JTA/JAE] : Factorisation du renommage de fichiers + Mise en place d'une condition pour détection de besoin de renommage

--- src/main/java/foldersContentOrdering/FolderOrderingEngine.java
+++ src/main/java/foldersContentOrdering/FolderOrderingEngine.java
@@ -7,6 +7,7 @@ import java.nio.file.Paths;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
@@ -38,24 +39,24 @@ public class FolderOrderingEngine {
 
 	}
 
+	//JTA/JAE : Prévoir de traiter cas où plus de 2 underscores dans le nom : se débarasser des premiers qui ne contiendraient que des chiffres
 	private static void traiterFichiers(String dossierDeTravailParam) {
 		final File dossierDeTravail = new File(dossierDeTravailParam);
 		if(FileUtils.isDirectory(dossierDeTravail)) {
 			final Collection<File> listOfFiles = FileUtils.listFiles(dossierDeTravail, null, false);
 			final int nombreDeFichiers = listOfFiles.size();
 			final String numberLength = String.valueOf(nombreDeFichiers);
-//			final Comparator<CustomFile> comparatorOlderOnesFirst = getFilesComparator();
-			 final List<CustomFile> orderedListOfFiles = listOfFiles.stream().map(f -> mapper.mapFromFileToCustomFile(f, recupererAttributsFichier(f))).sorted().toList();
+			 final List<CustomFile> orderedListOfFiles = listOfFiles.stream().map(f -> mapper.mapFromFileToCustomFile(f, recupererAttributsFichier(f))).sorted().collect(Collectors.toList());
 			 for(int i=0; i<orderedListOfFiles.size();++i) {
 				 final CustomFile loopFile = orderedListOfFiles.get(i);
 				 final String fileName = loopFile.getFichier().getName();
 				 final String fileAbsolutePath = loopFile.getFichier().getAbsolutePath();
 				 if(fileAbsolutePath.endsWith(fileName)) {
-					 String fileAbsolutePathToWorkOn = StringUtils.removeEnd(fileAbsolutePath, fileName);
-					 final String newFileName = StringUtils.leftPad(String.valueOf(i+1), numberLength.length(), ZERO_NUMBER) + UNDERSCORE_SIGN + fileName ;
-					 fileAbsolutePathToWorkOn  = fileAbsolutePathToWorkOn + newFileName;
-					 final File newFile = new File(fileAbsolutePathToWorkOn);
-					 loopFile.getFichier().renameTo(newFile);
+					 final String fileAbsolutePathToWorkOn = StringUtils.removeEnd(fileAbsolutePath, fileName);
+					 final String newFileNumber = String.valueOf(i+1);
+					 if(hasFileAWrongNumer(loopFile, newFileNumber)) {
+						 renameFile(numberLength, loopFile, fileAbsolutePathToWorkOn, newFileNumber);
+					 }
 				 }
 			 }
 		}
@@ -70,5 +71,24 @@ public class FolderOrderingEngine {
 			e.printStackTrace();
 		}
 		return resultat;
+	}
+
+	private static boolean hasFileAWrongNumer(CustomFile loopFileParam, String newFileNumberParam) {
+		boolean resultat = false;
+		if(loopFileParam != null && StringUtils.isNotBlank(newFileNumberParam)) {
+			final String fileNameLocal = loopFileParam.getFichier().getName();
+			final String[] splitLocal = StringUtils.split(fileNameLocal, UNDERSCORE_SIGN);
+			if(!StringUtils.equals(splitLocal[0], newFileNumberParam) || splitLocal.length < 1) {
+				resultat = true;
+			}
+		}
+		return resultat;
+	}
+
+	private static void renameFile(final String numberLength, final CustomFile loopFile,
+			String fileAbsolutePathToWorkOn, final String newFileNumber) {
+		final String newFileName = StringUtils.leftPad(newFileNumber, numberLength.length(), ZERO_NUMBER) + UNDERSCORE_SIGN + loopFile.getFichier().getName() ;
+		fileAbsolutePathToWorkOn  = fileAbsolutePathToWorkOn + newFileName;
+		loopFile.getFichier().renameTo(new File(fileAbsolutePathToWorkOn));
 	}
 }
